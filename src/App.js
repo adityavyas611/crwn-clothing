e469596dc75190ca743478bcdad278e21fef6285
Aditya Vyas <vyasaditya41@yahoo.in>
2019-12-25 18:52:57 +0530

    Created Project with CRA & Added files for homepage

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import HomePage from "./homepage.component";
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
