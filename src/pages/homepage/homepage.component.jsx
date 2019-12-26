7f5cd722eab73ce56e77c2d37d91660f72e36b63
Aditya Vyas <vyasaditya41@yahoo.in>
2019-12-26 17:14:15 +0530

    Destructuring Homepage Component into Separate Component

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './homepage.styles.scss';
+import Directory from '../../components/directory/directory.components';
+
+const HomePage = () => (
+   <div className='homepage'>
+       <Directory />
+  </div>
+);
+
+export default HomePage;
