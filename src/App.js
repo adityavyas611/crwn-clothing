c6e906b7c146a507321f2ca83fad7146ff3bb346
Aditya Vyas <vyasaditya41@yahoo.in>
2019-12-27 02:23:21 +0530

    Update menu-items to route using withRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { Route } from 'react-router-dom';
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+
+const Hatspage = () => (
+  <div>
+    <h1>Hats page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path='/' component={HomePage} />
+      <Route path='/hats' component={Hatspage} />
     </div>
   );
 }
